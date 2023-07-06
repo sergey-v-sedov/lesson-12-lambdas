6a43a6c34d8891b6a4760dcae1558a312e9f35a8
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-07-06 21:33:20 +0300

    Пример компаратора с лямбдой

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.ToDoubleBiFunction;
 
 public class Main {
@@ -16,5 +19,20 @@ public class Main {
         calculator.makeOperation((arg1, arg2) -> arg1 % arg2);
 
         calculator.makeOperation((arg1, arg2) -> arg1 / arg2);
+
+
+
+        List<String> list = new ArrayList<>(Arrays.asList("Красный--------", "Желтый----------------", "Зеленый"));
+        System.out.println(list);
+
+        list.sort((e1, e2) -> e1.compareTo(e2));
+        System.out.println(list);
+
+        list.sort((e1, e2) -> e1.length() - e2.length());
+        System.out.println(list);
+
+
+
+
     }
 }
