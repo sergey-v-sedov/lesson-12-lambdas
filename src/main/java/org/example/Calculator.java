d258ae8c38b1c8f539b5b2fdc7f5403e90617d8f
sergey-v-sedov <sergey.v.sedov@gmail.com>
2023-07-06 20:58:20 +0300

    Шаг 1. Вариант Калькулятора на ООП

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -8,16 +8,22 @@ public class Calculator {
     public void inputArguments(Double arg1, Double arg2) {
         this.arg1 = arg1;
         this.arg2 = arg2;
+        System.out.println("Аргументы введены");
+        validateArguments();
     }
 
     public void validateArguments() {
         if(arg1 == null || arg2 == null) {
             System.out.println("Значение аргументов не валидно");
+        } else {
+            System.out.println("Аргументы успешно провалидированы");
         }
     }
 
     public void makeOperation(Operation operation) {
         result = operation.calculate(arg1, arg2);
+        System.out.println("Операция " + operation + " успешно применена к аргументам");
+        printResult();
     }
 
     public void printResult() {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
+
         calculator.inputArguments(4.0, 5.0);
         Operation pow = new PowOperation();
         calculator.makeOperation(pow);
-        calculator.printResult();
 
         calculator.inputArguments(4.0, 5.0);
         Operation sum = new SumOperation();
         calculator.makeOperation(sum);
-        calculator.printResult();
     }
 }
